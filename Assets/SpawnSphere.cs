5f066d183006fefa4788e921bc8363c930cb01f0
Landships <mcaulay.ian2@gmail.com>
2016-10-02 16:27:48 -0700

    yolo 123 I don't know

---
+++ Assets/SpawnSphere.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSphere : MonoBehaviour {
+
+    public GameObject center;
+
+    public GameObject sphere;
+
+    public int minSpawnDistance = 30;
+    public int maxSpawnDistance = 50;
+    public int minSpeed = 1;
+    public int maxSpeed = 3;
+    public float spawnTime = .5f;
+
+    private float timeLeft;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        timeLeft = spawnTime;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeLeft <= 0) {
+
+            Vector3 spawn_loc = (Random.Range(minSpawnDistance, maxSpawnDistance) * Random.insideUnitSphere) + center.transform.position;
+            while (spawn_loc.y < center.transform.position.y) {
+                spawn_loc = Random.Range(minSpawnDistance, maxSpawnDistance) * Random.insideUnitSphere;
+            }
+
+            GameObject sphereClone = Instantiate(sphere, spawn_loc, sphere.transform.rotation) as GameObject;
+            sphereClone.SetActive(true);
+            //sphereClone.GetComponent<SpawnSphere> ().SetAc
+
+            timeLeft = spawnTime;
+
+        } else {
+
+            timeLeft -= Time.deltaTime;
+
+        }
+
+        
+
+        
+
+    }
+
+    
+
+}
---
+++ Assets/SphereMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMovement : MonoBehaviour {
+
+    public float minSpeed;
+    public float maxSpeed;
+    public GameObject center;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector3 direction = center.transform.position - transform.position;
+        float speed = Random.Range(minSpeed, maxSpeed);
+
+        transform.position = transform.position + (direction * Time.deltaTime * speed);
+
+    }
+}
